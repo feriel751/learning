cf623327a9d62460d314c9a700b3132e4bf02662
feryel gaaliche <ferielg088@gmail.com>
2022-02-23 15:51:27 +0100

    message we have done alll the fuching work

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ console.log("sayy hi ");
 console.log("have some relief");
 
 console.log("you are so stupid");
+console.log("affichier my work i have to go home ");
 
 
-
-
+console.log("affichier all my work ");
 
 
